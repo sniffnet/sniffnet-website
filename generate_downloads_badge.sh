5f08f7d2cf7f2394d42f0db75ff252b27d1e7e55
GyulyVGC <gyulyvgc99@gmail.com>
2025-08-12 17:12:44 +0200

    improve generate_downloads_badge.sh

--- generate_downloads_badge.sh
+++ generate_downloads_badge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GITHUB_COUNT=$(curl -s https://api.github.com/repos/gyulyvgc/sniffnet/releases | grep -E 'download_count' | cut '-d:' -f 2 | sed 's/,//g' | paste -s -d+ - | bc)
+GITHUB_COUNT=$(curl -s https://api.github.com/repos/gyulyvgc/sniffnet/releases | grep '"download_count"' | awk '{sum += $2} END {print sum}')
 echo "GITHUB_COUNT: $GITHUB_COUNT"
 
 CRATES_COUNT=$(curl -s https://crates.io/api/v1/crates/sniffnet | grep -E -o '"downloads":[0-9]*' | head -1 | cut '-d:' -f 2)
@@ -43,6 +43,6 @@ if ! [[ $DOWNLOADS_STRING =~ ^[1-9][0-9]{0,2}(\.[0-9]M|K)$ ]] ; then
        exit 1
 fi
 
-curl -o assets/img/downloads_badge.svg https://img.shields.io/badge/Downloads-$DOWNLOADS_STRING-blue?style=for-the-badge
+curl -s -f -o assets/img/downloads_badge.svg https://img.shields.io/badge/Downloads-$DOWNLOADS_STRING-blue?style=for-the-badge
 
 exit 0
