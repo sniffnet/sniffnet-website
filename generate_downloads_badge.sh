7adc978d4a2e291b71d83fca4501236f6e7e2603
GyulyVGC <gyulyvgc99@gmail.com>
2025-08-17 21:16:54 +0200

    improve generate_downloads_badge.sh for debugging

--- generate_downloads_badge.sh
+++ generate_downloads_badge.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-GITHUB_COUNT=$(curl -s https://api.github.com/repos/gyulyvgc/sniffnet/releases | grep '"download_count"' | awk '{sum += $2} END {print sum}')
-echo "GITHUB_COUNT: $GITHUB_COUNT"
+GITHUB_CURL=$(curl -s https://api.github.com/repos/gyulyvgc/sniffnet/releases)
+echo "---> GITHUB_CURL: $GITHUB_CURL"
+GITHUB_COUNT=$(echo "$GITHUB_CURL" | grep '"download_count"' | awk '{sum += $2} END {print sum}')
+echo "---> GITHUB_COUNT: $GITHUB_COUNT"
 
-CRATES_COUNT=$(curl -s https://crates.io/api/v1/crates/sniffnet | grep -E -o '"downloads":[0-9]*' | head -1 | cut '-d:' -f 2)
-echo "CRATES_COUNT: $CRATES_COUNT"
+CRATES_CURL=$(curl -s https://crates.io/api/v1/crates/sniffnet)
+echo "---> CRATES_CURL: $CRATES_CURL"
+CRATES_COUNT=$(echo "$CRATES_CURL" | grep -E -o '"downloads":[0-9]*' | head -1 | cut '-d:' -f 2)
+echo "---> CRATES_COUNT: $CRATES_COUNT"
 
-HOMEBREW_COUNT=$(curl -s https://github.com/Homebrew/homebrew-core/pkgs/container/core%2Fsniffnet | grep -A1 "Total downloads" | grep -o 'title="[0-9]*"' | cut '-d=' -f 2 | sed 's/"//g')
-echo "HOMEBREW_COUNT: $HOMEBREW_COUNT"
+HOMEBREW_CURL=$(curl -s https://github.com/Homebrew/homebrew-core/pkgs/container/core%2Fsniffnet)
+echo "---> HOMEBREW_CURL: $HOMEBREW_CURL"
+HOMEBREW_COUNT=$(echo "$HOMEBREW_CURL" | grep -A1 "Total downloads" | grep -o 'title="[0-9]*"' | cut '-d=' -f 2 | sed 's/"//g')
+echo "---> HOMEBREW_COUNT: $HOMEBREW_COUNT"
 
-GHCR_COUNT=$(curl -s https://github.com/gyulyvgc/sniffnet/pkgs/container/sniffnet | grep -A1 "Total downloads" | grep -o 'title="[0-9]*"' | cut '-d=' -f 2 | sed 's/"//g')
-echo "GHCR_COUNT: $GHCR_COUNT"
+GHCR_CURL=$(curl -s https://github.com/gyulyvgc/sniffnet/pkgs/container/sniffnet)
+echo "---> GHCR_CURL: $GHCR_CURL"
+GHCR_COUNT=$(echo "$GHCR_CURL" | grep -A1 "Total downloads" | grep -o 'title="[0-9]*"' | cut '-d=' -f 2 | sed 's/"//g')
+echo "---> GHCR_COUNT: $GHCR_COUNT"
 
 TOTAL_DOWNLOADS=$((GITHUB_COUNT+CRATES_COUNT+HOMEBREW_COUNT+GHCR_COUNT))
 echo "TOTAL_DOWNLOADS: $TOTAL_DOWNLOADS"
